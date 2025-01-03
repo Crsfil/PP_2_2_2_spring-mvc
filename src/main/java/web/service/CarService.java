768c3fae0bb1670ea522c6878e9c3792c8e824be
Crsfil <strizhovhotmail@gmail.com>
2025-01-04 01:12:28 +0300

    DONE

--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,4 +1,11 @@
 package web.service;
 
+import web.model.Car;
+
+import java.util.List;
+
 public interface CarService {
+//    @param счетчик машин
+//    @return список машин
+    List<Car> getCars(int count);
 }
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,4 +1,27 @@
 package web.service;
 
-public class CarServiceImpl {
+import org.springframework.stereotype.Service;
+import web.model.Car;
+
+import java.util.List;
+
+@Service
+public class CarServiceImpl implements CarService {
+    List<Car> cars = List.of(
+            new Car("Lada", "bakladjan", 2010),
+            new Car("Logan", "pink", 2014),
+            new Car("Geely", "blue", 2005),
+            new Car("Dodge", "Brown", 2019),
+            new Car("Volga", "white", 2008)
+    );
+
+
+
+    @Override
+    public List<Car> getCars(int count) {
+        if (count >= cars.size()) {
+            return cars;
+        }
+        return cars.subList(0, count);
+    }
 }
