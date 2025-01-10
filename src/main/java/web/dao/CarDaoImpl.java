6422a5497a0e13f5f44f198e53448b725f35ec5e
Crsfil <strizhovhotmail@gmail.com>
2025-01-10 22:02:56 +0300

    DONE

---
+++ src/main/java/web/dao/CarDaoImpl.java
@@ -0,0 +1,24 @@
+package web.dao;
+
+import web.model.Car;
+
+import java.util.List;
+
+public class CarDaoImpl implements CarDao {
+
+    List<Car> cars = List.of(
+            new Car("Lada", "bakladjan", 2010),
+            new Car("Logan", "pink", 2014),
+            new Car("Geely", "blue", 2005),
+            new Car("Dodge", "Brown", 2019),
+            new Car("Volga", "white", 2008)
+    );
+
+    @Override
+    public List<Car> getCars(int count) {
+        if (count >= cars.size()) {
+            return cars;
+        }
+        return cars.subList(0, count);
+    }
+}
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,27 +1,20 @@
 package web.service;
 
 import org.springframework.stereotype.Service;
+import web.dao.CarDao;
+import web.dao.CarDaoImpl;
 import web.model.Car;
 
 import java.util.List;
 
 @Service
 public class CarServiceImpl implements CarService {
-    List<Car> cars = List.of(
-            new Car("Lada", "bakladjan", 2010),
-            new Car("Logan", "pink", 2014),
-            new Car("Geely", "blue", 2005),
-            new Car("Dodge", "Brown", 2019),
-            new Car("Volga", "white", 2008)
-    );
 
 
 
     @Override
     public List<Car> getCars(int count) {
-        if (count >= cars.size()) {
-            return cars;
-        }
-        return cars.subList(0, count);
+        CarDao carDao = new CarDaoImpl();
+        return carDao.getCars(count);
     }
 }
